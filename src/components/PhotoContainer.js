bcaa78f48fd8734e9e1aedf6278ab21faa95de98
maribelcuales <maribelcuales@gmail.com>
2022-01-18 21:59:53 +0800

    Added PhotoContainer component and env file.

---
+++ src/components/PhotoContainer.js
@@ -0,0 +1,26 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import PhotoCard from "./NasaPhotoCard";
+
+function PhotoContainer() {	
+	const [nasaPhoto, setNasaPhoto] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get(process.env.API_URL)
+			.then(response => {
+				console.log(response.data);
+				setNasaPhoto(response.data);
+			})
+			.catch(error => {
+				console.log('Error getting data from NASA.', error)
+			});
+	}, []);
+
+  return (
+		<div className="photo-container">
+			<PhotoCard />
+		</div>
+	)};
+
+export default PhotoContainer; 
